2b437cf3e5cbf6c89b9a26fd5baf5336da577355
Mattias Larsson <mattias.larsson75@outlook.com>
2017-02-08 14:40:54 +0100

    App working

--- src/main/java/com/example/controllers/StationController.java
+++ src/main/java/com/example/controllers/StationController.java
@@ -1,8 +1,26 @@
 package com.example.controllers;
 
+import com.example.entities.Station;
+import com.example.repositories.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * <h1>Created by Mattias on 2017-02-08.</h1>
  */
-
+@RestController
+@RequestMapping("/station")
 public class StationController {
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @GetMapping
+    public List<Station> getAllStations() {
+        return stationRepository.findAll();
+    }
 }
