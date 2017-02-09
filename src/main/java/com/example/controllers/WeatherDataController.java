c1d453268d3a39ac0fcf1f17abfb5fdf6b717e2f
jolindse@hotmail.com <jolindse@hotmail.com>
2017-02-09 10:12:08 +0100

    Added Station unit tests.

---
+++ src/main/java/com/example/controllers/WeatherDataController.java
@@ -0,0 +1,45 @@
+package com.example.controllers;
+
+import com.example.entities.WeatherData;
+import com.example.repositories.WeatherDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * <h1>Created by Mattias on 2017-02-08.</h1>
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:63342")
+@RequestMapping("/weatherdata")
+public class WeatherDataController {
+
+    @Autowired
+    private WeatherDataRepository weatherDataRepository;
+
+    @GetMapping
+    public List<WeatherData> getAllWeatherData() {
+        return weatherDataRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public WeatherData getASpecificWeatherData(@PathVariable int id) {
+        return weatherDataRepository.findOne(id);
+    }
+
+    @PostMapping
+    public WeatherData createNewWeatherData(@RequestBody WeatherData weatherData) {
+        return weatherDataRepository.save(weatherData);
+    }
+
+    @PutMapping
+    public WeatherData updateWeatherData(@RequestBody WeatherData weatherData) {
+        return weatherDataRepository.save(weatherData);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteWeatherData(@PathVariable int id) {
+        weatherDataRepository.delete(id);
+    }
+}
---
+++ src/test/java/com/example/StationTest.java
@@ -0,0 +1,56 @@
+package com.example;
+
+import com.example.entities.Station;
+import com.example.repositories.StationRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+/**
+ * Created by juan on 2017-02-09.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class StationTest {
+
+    @Autowired
+    private StationRepository repo;
+
+    private Station testStation;
+
+    @Before
+    public void initTest() {
+        testStation = new Station();
+    }
+
+    @Test
+    public void testSaveStation() {
+        Station returnedStation = repo.save(testStation);
+        assertTrue("Stations doesn't match", returnedStation != null);
+    }
+
+    @Test
+    public void testChangeStation() {
+        float lat = 11.2f;
+        float lng = 12.1f;
+        String pass = "test";
+
+        testStation.setAdmin(true);
+        testStation.setLat(11.2f);
+        testStation.setLng(12.1f);
+        testStation.setPassword("test");
+        repo.save(testStation);
+
+        assertTrue("Admin rights not properly saved",testStation.isAdmin());
+        assertTrue("Latitude not matching",testStation.getLat() == lat);
+        assertTrue("Longitudes not matching", testStation.getLng() == lng);
+        assertTrue("Passwords doesn't match",testStation.getPassword().equals(pass));
+        assertNotNull(testStation.getId());
+    }
+}
