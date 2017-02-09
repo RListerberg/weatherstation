d6dc2c93cf9f7f5c077ad83d2cad2de27f2a6536
RListerberg <Robin_listerberg@hotmail.com>
2017-02-09 15:52:20 +0100

    Started buildning login form

--- src/main/js/src/components/login/Login.js
+++ src/main/js/src/components/login/Login.js
@@ -3,9 +3,25 @@ import './Login.css';
 
 export default class Login extends Component {
 
+
+    handleLogin = (event) =>{
+
+    }
+
     render() {
         return (
             <div id="login-root">
+
+                <div id="login-form">
+                    <label><b>StationsId</b></label>
+                    <input type="text" placeholder="stationsId" name="uname" />
+                    <label><b>Password</b></label>
+                    <input type="text" placeholder="password" name="pword"/>
+                    <div>
+                        <button type="button" class="cancelBut">Cancel</button>
+                        <button type="button" class="LoginBut" onChange={this.handleLogin}>Login</button>
+                    </div>
+                </div>
 
             </div>
         )
