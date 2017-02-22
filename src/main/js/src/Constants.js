88627e0607678a9c5c1d002b45c5784043092ada
Sunnari <jsunnari@gmail.com>
2017-02-22 10:11:45 +0100

    Last 5 posts for weatherstation.

--- src/main/js/src/Constants.js
+++ src/main/js/src/Constants.js
@@ -20,3 +20,42 @@ export const loginURL = "/login/";
 
 export var loggedIn = false;
 export var jwtToken = "";
+
+/***********************************************************************************************************************
+ * COMPONENT-DATA
+ **********************************************************************************************************************/
+export const windValues = [
+    {id: 1, value: "North", text: "North"},
+    {id: 2, value: "East", text: "East"},
+    {id: 3, value: "South", text: "South"},
+    {id: 4, value: "West", text: "West"},
+    {id: 5, value: "Northeast", text: "Northeast"},
+    {id: 6, value: "Northwest", text: "Northwest"},
+    {id: 7, value: "Southeast", text: "Southeast"},
+    {id: 8, value: "Southwest", text: "Southwest"},
+];
+export const cloudValues = [
+    {id: 1, value: "Fibratus", text: "Fibratus"},
+    {id: 2, value: "Capillatus", text: "Capillatus"},
+    {id: 3, value: "Castellanus", text: "Castellanus"},
+    {id: 4, value: "Lenticularis", text: "Lenticularis"},
+    {id: 5, value: "Mediocris", text: "Mediocris"},
+    {id: 6, value: "Spissatus", text: "Spissatus"},
+    {id: 7, value: "Calvus", text: "Calvus"},
+    {id: 8, value: "Nebulosus", text: "Nebulosus"},
+    {id: 9, value: "Uncinus", text: "Uncinus"},
+    {id:10, value: "Humilis", text: "Humilis"},
+    {id:11, value: "Stratiformis", text: "Stratiformis"},
+    {id:12, value: "Fractus", text: "Fractus"},
+    {id:13, value: "Floccus", text: "Floccus"}
+];
+export const cloudCoverageValues = [
+    {id: 1, value: 1, text: "1/8"},
+    {id: 2, value: 2, text: "2/8"},
+    {id: 3, value: 3, text: "3/8"},
+    {id: 4, value: 4, text: "4/8"},
+    {id: 5, value: 5, text: "5/8"},
+    {id: 6, value: 6, text: "6/8"},
+    {id: 7, value: 7, text: "7/8"},
+    {id: 8, value: 8, text: "8/8"},
+];
--- src/main/js/src/index.js
+++ src/main/js/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root/Root';
+
 import Login from './components/login/Login';
 import StatsRoot from './components/statsroot/StatsRoot';
 import './index.css';
 
 ReactDOM.render(
-  <StatsRoot />,
+  <Root />,
   document.getElementById('root')
 );
