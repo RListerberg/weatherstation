4a93c7f9d9518037de6c0e6645b3ea58bc70da29
Sunnari <jsunnari@gmail.com>
2017-02-22 09:27:52 +0100

    Merge to master

--- src/main/js/src/index.js
+++ src/main/js/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root/Root';
 //import Login from './components/login/Login';
-
 import './index.css';
 
 ReactDOM.render(
